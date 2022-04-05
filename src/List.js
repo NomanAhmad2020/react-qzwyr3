ab8e7c04105d07bef2f77b607acdb544d5312b63
NomanAhmad2020 <nomanpak@gmail.com>
2022-04-05 20:16:16 +0500

    1 file modified

--- src/List.js
+++ src/List.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 function List({}) {
   var nameList = [
-    { id: "01", firstname: "Ali", lastname: "Muhammad" },
-    { id: "02", firstname: "Zaryab", lastname: "Khan" },
-    { id: "03", firstname: "Rizwan", lastname: "Kush" },
-    { id: "04", firstname: "Rashid", lastname: "Hussain" }
+    { id: '01', firstname: 'Ali', lastname: 'Muhammad' },
+    { id: '02', firstname: 'Zaryab', lastname: 'Khan' },
+    { id: '03', firstname: 'Rizwan', lastname: 'Kush' },
+    { id: '04', firstname: 'Rashid', lastname: 'Hussain' },
   ];
   const itemList = nameList.map((item) => (
     <li>
-       {item.lastname}/ {item.firstname}
+      {item.lastname}/ {item.firstname}
     </li>
   ));
   return (
@@ -19,5 +19,5 @@ function List({}) {
       {itemList}
     </div>
   );
-} export default List;
-
+}
+export default List;
